ca811e32f4c2ee6c8111c2e431e61494222c509f
PlaifahRungjjrathanakul <plaifah.rung@bumail.net>
2025-03-29 22:43:05 +0700

    Brick Door

--- Assets/Scripts/SlimeMovement.cs
+++ Assets/Scripts/SlimeMovement.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class SlimeMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5f;
+    public float pushForce = 10f;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * moveSpeed);
+
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 2f))
+            {
+                if (hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(transform.forward * pushForce, ForceMode.Impulse);
+                }
+            }
+        }
     }
 }
